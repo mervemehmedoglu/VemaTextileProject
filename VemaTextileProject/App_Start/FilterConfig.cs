1db4841aa48b2f82e315c8ec93ac2c2da75e99d2
Merve Mehmedoğlu <64040142+mervemehmedoglu@users.noreply.github.com>
2020-11-04 16:38:08 +0300

    Add project files.

---
+++ VemaTextileProject/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace VemaTextileProject
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
