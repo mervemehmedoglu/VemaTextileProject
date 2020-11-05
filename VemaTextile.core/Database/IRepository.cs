6227af548bc32a6f59e3411ac20b3ea44274fe8e
oguzhan celik <ogzzcelik@gmail.com>
2020-11-05 17:56:23 +0300

    oguzhan

---
+++ VemaTextile.core/Database/IRepository.cs
@@ -0,0 +1,26 @@
+﻿using VemaTextile.Entity.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq.Expressions;
+
+
+
+
+namespace VemaTextile.core.Database
+{
+    public interface IRepository<T>
+        where T : EntityBase, new()
+    {
+        void Add(T entity);
+        void Delete(int id);
+        void Update(T entity);
+        IEnumerable<T> GetAll();
+        IQueryable<T> WhereQueryable(Expression<Func<T, bool>> lamda);
+        IEnumerable<T> Where(Expression<Func<T, bool>> lamda);
+        T GetObject(Expression<Func<T, bool>> lamda);
+    }
+    
+}
