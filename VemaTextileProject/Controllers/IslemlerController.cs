51fc15da39b6cc3c0b1f7449546bc82410fc68a7
oguzhan celik <ogzzcelik@gmail.com>
2020-11-19 11:59:46 +0300

    controllerlar eklendi

---
+++ VemaTextileProject/Controllers/IslemlerController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VemaTextileProject.Controllers
+{
+    public class IslemlerController : Controller
+    {
+        // GET: Islemler
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
